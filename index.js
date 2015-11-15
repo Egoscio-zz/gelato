45b934a998c78f4be7f8e182d052aead4f9ee267
Egoscio <theiqboy@gmail.com>
2015-11-15 14:21:59 -0800

    NPM Friendly
    Added package.json and index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * index.js - The heart of Gelato
+ */
+
+module.exports = {
+	Command: require('./lib/command.js'),
+	Minecraft: require('./lib/minecraft.js'),
+	Netrix: require('./lib/netrix.js'),
+	Proxy: require('./lib/proxy.js'),
+	Toolbox = require('./lib/toolbox.js')
+};
